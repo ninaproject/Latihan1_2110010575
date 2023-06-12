2003d3138464a0ffaa9452859d1da415e0be9c20
ninaproject <ninabrays12@gmail.com>
2023-06-11 22:53:34 -0900

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hitung;
 
-/**
- *
- * @author User
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args){
+        //membuat objek
+        Matematika nina = new Matematika(7,5);
+        
+        System.out.println("Hasil penjumlahan: "+nina.setPenjumlahan());
+        System.out.println("Hasil pengurangan: "+nina.setPengurangan());
+        System.out.println("Hasil perkalian: "+nina.setPerkalian());
+        System.out.println("Hasil pembagian: "+nina.setPembagian());
+    }
 }
